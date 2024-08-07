79980be0254bed8182de23b9417e26e931c698fd
themanvendra08 <manvendra.singh@zysk.tech>
2024-08-08 00:31:42 +0530

    Changes the response data

--- index.js
+++ index.js
@@ -2,61 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const specializations = [
-  {
-    id: '1',
-    name: 'Cardiology',
-    description: 'Specializes in diagnosing and treating heart conditions.',
-  },
-  {
-    id: '2',
-    name: 'Neurology',
-    description: 'Deals with disorders of the nervous system.',
-  },
-  {
-    id: '3',
-    name: 'Orthopedics',
-    description:
-      'Focuses on the musculoskeletal system, including bones, joints, and muscles.',
-  },
-  {
-    id: '4',
-    name: 'Pediatrics',
-    description:
-      'Provides medical care for infants, children, and adolescents.',
-  },
-  {
-    id: '5',
-    name: 'Dermatology',
-    description: 'Treats skin conditions and diseases.',
-  },
-  {
-    id: '6',
-    name: 'Psychiatry',
-    description:
-      'Specializes in diagnosing and treating mental health disorders.',
-  },
-  {
-    id: '7',
-    name: 'Oncology',
-    description: 'Focuses on the treatment of cancer.',
-  },
-  {
-    id: '8',
-    name: 'Gastroenterology',
-    description: 'Deals with disorders of the digestive system.',
-  },
-  {
-    id: '9',
-    name: 'Endocrinology',
-    description: 'Specializes in hormone-related diseases.',
-  },
-  {
-    id: '10',
-    name: 'Ophthalmology',
-    description: 'Focuses on eye and vision care.',
-  },
-];
+const specializations = {
+  Cardiologist: 'CA',
+  Neurologist: 'NE',
+  Orthopedist: 'OR',
+  Pediatrician: 'PE',
+  Dermatologist: 'DE',
+  Psychiatrist: 'PS',
+  Oncologist: 'ON',
+  Gastroenterologist: 'GA',
+  Endocrinologist: 'EN',
+  Ophthalmologist: 'OP',
+  Normal_Check_Ups: 'NC',
+  General_Check_Ups: 'GC',
+};
 
 app.get('/specializations', (req, res) => {
   res.json(specializations);
