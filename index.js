3819d1a4e6bbf10b093aa8e14ceb7a201cf7b7a1
themanvendra08 <manvendra.singh@zysk.tech>
2024-08-08 11:38:08 +0530

    Add get api to fetch symptoms

--- index.js
+++ index.js
@@ -13,12 +13,69 @@ const specializations = {
   Gastroenterologist: 'GA',
   Endocrinologist: 'EN',
   Ophthalmologist: 'OP',
-  Normal_Check_Ups: 'NC',
-  General_Check_Ups: 'GC',
+  'Normal Check Ups': 'NC',
+  'General Check Ups': 'GC',
+};
+
+const symptoms = {
+  Fever: 'FE',
+  Cough: 'CO',
+  Headache: 'HE',
+  Fatigue: 'FA',
+  'Sore Throat': 'ST',
+  'Shortness of Breath': 'SB',
+  'Runny Nose': 'RN',
+  'Muscle Pain': 'MP',
+  'Joint Pain': 'JP',
+  Chills: 'CH',
+  Sweating: 'SW',
+  Nausea: 'NA',
+  Vomiting: 'VO',
+  Diarrhea: 'DI',
+  Constipation: 'CS',
+  'Abdominal Pain': 'AP',
+  'Chest Pain': 'CP',
+  Palpitations: 'PA',
+  Dizziness: 'DZ',
+  'Blurred Vision': 'BV',
+  Tremors: 'TR',
+  Rash: 'RA',
+  Itching: 'IT',
+  Swelling: 'SW',
+  'Weight Loss': 'WL',
+  'Weight Gain': 'WG',
+  'Hair Loss': 'HL',
+  'Increased Thirst': 'IT',
+  'Increased Urination': 'IU',
+  'Difficulty Swallowing': 'DS',
+  Hoarseness: 'HO',
+  'Ear Pain': 'EP',
+  'Hearing Loss': 'HL',
+  Nosebleed: 'NB',
+  'Sinus Pressure': 'SP',
+  Wheezing: 'WH',
+  Heartburn: 'HB',
+  'Difficulty Breathing': 'DB',
+  Anxiety: 'AN',
+  Depression: 'DE',
+  Insomnia: 'IN',
+  'Memory Loss': 'ML',
+  Confusion: 'CF',
+  Seizures: 'SZ',
+  Fainting: 'FA',
+  'Back Pain': 'BP',
+  'Neck Pain': 'NP',
+  'Shoulder Pain': 'SP',
+  'Knee Pain': 'KP',
+  'Hip Pain': 'HP',
 };
 
 app.get('/specializations', (req, res) => {
   res.json(specializations);
+});
+
+app.get('/symptoms', (req, res) => {
+  res.json(symptoms);
 });
 
 app.listen(port, () => {
